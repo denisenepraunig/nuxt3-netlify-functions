4777d86fca551f25a43242acc68808a1b03f1412
Denise Nepraunig <denise.nepraunig@jamf.com>
2024-04-07 13:46:50 +0200

    direct url

--- server/api/call-function.ts
+++ server/api/call-function.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   // ... Do whatever you want here
-  const whatever = await $fetch('/.netlify/functions/hello')
+  const whatever = await $fetch('https://nuxt3-netlify-functions.netlify.app/.netlify/functions/hello')
   console.log("🐥", whatever)
   return whatever
 })
