14f450981b5addc6a55df0366f949e2caf7be2dd
Denise Nepraunig <denise.nepraunig@jamf.com>
2024-04-07 21:58:27 +0200

    display geo information

---
+++ pages/geo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Geo</h1>
+    <div v-if="data">
+      <pre>{{ data }}</pre>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { data } = await useFetch('/api/geo')
+console.log('data:', data.value)
+</script>
+
+<style></style>
--- server/api/geo.get.ts
+++ server/api/geo.get.ts
@@ -1,4 +1,6 @@
 export default defineEventHandler(async (event) => {
+  // const geoinformation = await $fetch('http://localhost:8888/.netlify/functions/geo')
   const geoinformation = await $fetch('https://nuxt3-netlify-functions.netlify.app/.netlify/functions/geo')
+  // console.log("🐥🐥", geoinformation)
   return geoinformation
 })
