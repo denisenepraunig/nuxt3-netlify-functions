cf0a59b0e468eb4ab591a542d79ef7e3a8e55a57
Denise Nepraunig <denise.nepraunig@jamf.com>
2024-04-07 13:04:40 +0200

    center index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h1>Home page</h1>
+  <div class="centered">
+    <h1>Meow!</h1>
+    <img src="/original-cat/purple-cat.png" alt="Purple cat logo" width="256" />
   </div>
 </template>
 
@@ -8,4 +9,10 @@
 
 </script>
 
-<style></style>
+<style scoped>
+.centered {
+  margin: 20px;
+  margin-right: 20px;
+  text-align: center;
+}
+</style>
